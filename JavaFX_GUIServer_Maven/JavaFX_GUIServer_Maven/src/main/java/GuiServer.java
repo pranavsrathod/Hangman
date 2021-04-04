2e79f9ea16a18715f19bb6b54dfced37c4b9bc36
Parth-09 <71151806+Parth-09@users.noreply.github.com>
2021-04-04 18:13:03 -0500

    Add files via upload

---
+++ JavaFX_GUIServer_Maven/JavaFX_GUIServer_Maven/src/main/java/GuiServer.java
@@ -0,0 +1,116 @@
+
+import java.util.HashMap;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class GuiServer extends Application{
+
+	
+	TextField s1,s2,s3,s4, c1;
+	Button serverChoice,clientChoice,b1;
+	HashMap<String, Scene> sceneMap;
+	GridPane grid;
+	HBox buttonBox;
+	VBox clientBox;
+	Scene startScene;
+	BorderPane startPane;
+	Server serverConnection;
+	Client clientConnection;
+	
+	ListView<String> listItems, listItems2;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		c1 = new TextField();
+		primaryStage.setTitle("Welcome to the Server");
+		Label label = new Label("Enter Port Number:");
+		this.serverChoice = new Button("Login");
+		buttonBox = new HBox(10, label, serverChoice);
+		
+		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
+											primaryStage.setTitle("This is the Server");
+				serverConnection = new Server(data -> {
+					Platform.runLater(()->{
+						listItems.getItems().add(data.toString());
+					});
+
+				},Integer.parseInt(c1.getText()));
+											
+		});
+		VBox vbox = new VBox(10, buttonBox, c1);
+		vbox.setAlignment(Pos.CENTER);
+		startPane = new BorderPane();
+		startPane.setCenter(vbox);
+		
+		startScene = new Scene(startPane, 400,400);
+		
+		listItems = new ListView<String>();
+		listItems2 = new ListView<String>();
+//		b1 = new Button("Send");
+//		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
+		
+		sceneMap = new HashMap<String, Scene>();
+		
+		sceneMap.put("server",  createServerGui());
+//		sceneMap.put("client",  createClientGui());
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+		
+		 
+		
+		primaryStage.setScene(startScene);
+		primaryStage.show();
+		
+	}
+	
+	public Scene createServerGui() {
+		
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(70));
+		pane.setStyle("-fx-background-color: coral");
+		
+		pane.setCenter(listItems);
+	
+		return new Scene(pane, 500, 400);
+		
+		
+	}
+	
+	public Scene createClientGui() {
+		
+		clientBox = new VBox(10, c1,b1,listItems2);
+		clientBox.setStyle("-fx-background-color: blue");
+		return new Scene(clientBox, 400, 300);
+		
+	}
+
+}
